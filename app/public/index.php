a2a3f37b279bc08e91a14cbe47e8a25ba33eb8ad
Serhii Shynkarenko <serhii.shynkarenko@videoslots.com>
2023-03-10 20:27:15 +0200

    multi-stage-build: done

--- app/public/index.php
+++ app/public/index.php
@@ -2,6 +2,8 @@
 
 use App\Repository\TranslationRepository;
 
+phpinfo();
+
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 $client = \Symfony\Component\Cache\Adapter\RedisAdapter::createConnection(
