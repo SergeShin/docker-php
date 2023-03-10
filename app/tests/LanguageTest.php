3ef48493e5aec6e1b13d238ed98976c2a28b240d
Serhii Shynkarenko <serhii.shynkarenko@videoslots.com>
2023-03-10 20:43:49 +0200

    pushing-images: done

---
+++ app/tests/LanguageTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests;
+
+use PHPUnit\Framework\TestCase;
+
+final class LanguageTest extends TestCase
+{
+    public function testItWorks(): void
+    {
+        $this->assertTrue(true);
+    }
+}
